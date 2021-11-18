49e2a6ebf63fff99ae770584467a1a4e8a9de740
pao0318 <70106499+pao0318@users.noreply.github.com>
2021-11-18 22:56:45 +0530

    added signin

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,9 +1,12 @@
-const mongoose=require("mongoose");
-mongoose.connect(process.env.MONGO_URL,{
+const mongoose = require("mongoose");
+mongoose
+  .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-  }).then(()=>{
+    useUnifiedTopology: true,
+  })
+  .then(() => {
     //console.log(`succesful connection`);
-  }).catch((e)=>{
-      console.log(`mongoose connection error ${e}`);
+  })
+  .catch((e) => {
+    console.log(`mongoose connection error ${e}`);
   });
--- src/models/registers.js
+++ src/models/registers.js
@@ -1,45 +1,43 @@
-const mongoose=require('mongoose');
-const userSchema=new mongoose.Schema({
-    name:{
-        type:String,
-        required:true
-    },
-    email:{
-        type:String,
-        required:true
-    },
-    password:{
-        type:String,
-        required:true
-    },
-    phone:{
-        type:Number,
-        required:true
-    },
-    role:{
-        type:String,
-        required:true
-    },
-    interest:{
-        type:[String],
-        required:true
-    },
-    work:{
-        type:String
-    },
-    company:{
-        type:String,
-    },
-    experience:{
-        type:Number
-    },
-    img:
-    {
-        data: Buffer,
-        contentType: String
-    }
+const mongoose = require("mongoose");
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: Number,
+    required: true,
+  },
+  role: {
+    type: String,
+    required: true,
+  },
+  interest: {
+    type: [String],
+    required: true,
+  },
+  work: {
+    type: String,
+  },
+  company: {
+    type: String,
+  },
+  experience: {
+    type: Number,
+  },
+  img: {
+    data: Buffer,
+    contentType: String,
+  },
+});
 
-})
-
-const docobject=new mongoose.model("Register",userSchema);
-module.exports=docobject;
+const docobject = new mongoose.model("Register", userSchema);
+module.exports = docobject;
