0e75f27565f5b6f9ce252873d227cdc02263d643
parwinakhtari <parwinakhtari7@gmail.com>
2021-11-20 23:06:13 +0530

    profile authentication

--- src/app.js
+++ src/app.js
@@ -1,19 +1,14 @@
 const express = require("express");
 const app = express();
+const cookieParser = require("cookie-parser");
 const path = require("path");
-const fs = require("fs");
-const multer = require("multer");
-
-require("dotenv/config");
-require("./db/conn");
-
-const RegisterSchema = require("./models/registers");
-
-const port = process.env.PORT || 3000;
+var env = require("dotenv/config");
+var mongoose = require('mongoose');
+var session = require('express-session');
+var MongoStore = require('connect-mongo');
 
 const static_path = path.join(__dirname, "../public");
 const template_path = path.join(__dirname, "../templates/views");
-//const partials_path=path.join(__dirname,"../templates/partials");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -22,111 +17,53 @@ app.use(express.static(static_path));
 app.set("view engine", "ejs");
 app.set("views", template_path);
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads");
-  },
-  filename: (req, file, cb) => {
-    cb(null, file.fieldname + "-" + Date.now());
-  },
-});
-const upload = multer({ storage: storage });
-
-app.get("/", (req, res) => {
-  res.render("index");
-});
-app.get("/signupmentee", (req, res) => {
-  res.render("signupmentee");
-});
-app.get("/signupmentor", (req, res) => {
-  res.render("signupmentor");
-});
-app.get("/signupbtn", (req, res) => {
-  res.render("signupbtn");
-});
-app.get("/login", (req, res) => {
-  res.render("sign-in");
-});
-
-//create new user in db for mentee
-app.post("/registermentee", async (req, res) => {
-  try {
-    const registeruser = await new RegisterSchema({
-      name: req.body.name,
-      password: req.body.password,
-      phone: req.body.phone,
-      email: req.body.email,
-      role: "mentee",
-      interest: req.body.interest.split(","),
-    });
-    registeruser.save();
-    //mentor matching query
-    const interestarray = req.body.interest.split(",");
-    let queryarr = [];
-    for (let index = 0; index < interestarray.length; index++) {
-      queryarr.push({ interest: interestarray[index] });
-    }
-    let query = { $and: [{ $or: queryarr }, { role: "mentor" }] };
-    console.log(query);
-    const promiseresult=await RegisterSchema.find(query);
-    console.log(promiseresult[0].name);
-    res.status(201).render("index");
-  } catch (error) {
-    res.status(400).render("signupmentee");
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}, (err) => {
+  if (!err) {
+    console.log('MongoDB Connection Succeeded.');
+  } else {
+    console.log('Error in DB connection : ' + err);
   }
 });
 
-//create new user in db for mentor
-app.post("/registermentor", upload.single("image"), async (req, res) => {
-  try {
-    const registeruser = await new RegisterSchema({
-      name: req.body.name,
-      password: req.body.password,
-      phone: req.body.phone,
-      email: req.body.email,
-      role: "mentor",
-      interest: req.body.interest.split(","),
-      work: req.body.work,
-      company: req.body.company,
-      experience: req.body.experience,
-      img: {
-        data: fs.readFileSync(
-          path.join(__dirname, `../uploads/${req.file.filename}`)
-        ),
-        contentType: "image/png",
-      },
-    });
-    registeruser.save();
-    console.log(registeruser.interest);
-    res.status(201).render("index");
-  } catch (error) {
-    res.status(400).send(error);
-  }
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
 });
 
-// Login check
-app.post("/login", async (req, res) => {
-  try {
-    const email = req.body.email;
-    const password = req.body.password;
+// creating 24 hours from milliseconds
+const oneDay = 1000 * 60 * 60 * 24;
+app.use(session({
+  secret: 'thisismysecrctekeyfhrgfgrfrty84fwir767',
+  resave: true ,
+  saveUninitialized: false,
+  cookie: { maxAge: oneDay },
+  store: MongoStore.create({
+    mongoUrl: process.env.MONGO_URL //(URL FROM.env file)
+  })
+}));
 
-    const useremail = await RegisterSchema.findOne({ email: email });
-    if (useremail.password === password) {
-      res.status(201).render("index");
-    } else {
-      res.redirect("/login");
-    }
-  } catch (error) {
-    console.log("invalid login details");
-    res.redirect("/login");
-  }
+
+
+var index = require('./route.js');
+app.use('/', index);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  var err = new Error('Ooops!! Requested Pasge Not Found');
+  err.status = 404;
+  next(err);
 });
 
-//dashboard render
-app.get("/dashboard", (req, res) => {
-  res.render("dashboard");
+// error handler
+// define as the last app.use callback
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.send(err.message);
 });
-
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Listening at port http://localhost:${port}/login`);
+  console.log(`Listening at port http://localhost:${port}/`);
 });
---
+++ src/route.js
@@ -0,0 +1,169 @@
+const express = require("express");
+var router = express.Router();
+const path = require("path");
+const fs = require("fs");
+const multer = require("multer");
+
+require("dotenv/config");
+const RegisterSchema = require("./models/registers");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + "-" + Date.now());
+  },
+});
+const upload = multer({ storage: storage });
+
+router.get("/", (req, res) => {
+  RegisterSchema.findOne({_id:req.session.userId},function(err,data){
+		if(!data){
+      res.render("sign-in",{created:""});
+		}else{
+			console.log("found sesion");
+      res.render("index");
+		}
+});
+});
+router.get("/signupmentee", (req, res) => {
+  res.render("signupmentee");
+});
+router.get("/signupmentor", (req, res) => {
+  res.render("signupmentor");
+});
+router.get("/signupbtn", (req, res) => {
+  res.render("signupbtn");
+});
+
+//create new user in db for mentee
+router.post("/registermentee", async (req, res) => {
+  try {
+    // //mentor matching query
+    // const interestarray = req.body.interest.split(",");
+    // let queryarr = [];
+    // for (let index = 0; index < interestarray.length; index++) {
+    //   queryarr.push({ interest: interestarray[index] });
+    // }
+    // // let query = { $and: [{ $or: queryarr }, { role: "mentor" }] };
+    // // console.log(query);
+    // // const promiseresult=await RegisterSchema.find(query);
+    // // console.log(promiseresult[0].name);
+    var newPerson = new RegisterSchema({
+      name: req.body.name,
+      password: req.body.password,
+      phone: req.body.phone,
+      email: req.body.email,
+      role: "mentee",
+      interest: req.body.interest.split(","),
+    });
+    newPerson.save(function (err, Person) {
+      if (err){
+      console.log(err);
+      console.log(newPerson);
+    res.render("signupmentee");
+      }
+      else console.log("Success sign up mentee");
+    });
+    res.status(201).render("sign-in",{created:"Account created succesfully!"});
+
+  } catch (error) {
+    res.status(400).render("signupmentee");
+  }
+});
+
+//create new user in db for mentor
+router.post("/registermentor", upload.single("image"), async (req, res) => {
+  try {
+    const registeruser = await new RegisterSchema({
+      name: req.body.name,
+      password: req.body.password,
+      phone: req.body.phone,
+      email: req.body.email,
+      role: "mentor",
+      interest: req.body.interest.split(","),
+      work: req.body.work,
+      company: req.body.company,
+      experience: req.body.experience,
+      img: {
+        data: fs.readFileSync(
+          path.join(__dirname, `../uploads/${req.file.filename}`)
+        ),
+        contentType: "image/png",
+      },
+    });
+    registeruser.save();
+    console.log(registeruser.name);
+    registeruser.save(function (err, Person) {
+      if (err){
+      console.log(err);
+      res.render("signupmentor");
+      }
+      else console.log("Success signup mentor");
+    });
+    res.status(201).render("sign-in",{created:"Account created succesfully!"});
+
+
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+// Login check
+router.post("/login", async (req, res,next) => {
+  try {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const useremail = await RegisterSchema.findOne({ email: email });
+    if (useremail.password === password) {
+      req.session.userId = useremail._id;
+      console.log(useremail.name);
+      res.render("index");
+    } else {
+      console.log("invalid password");
+      res.render("sign-in",{created:""});
+    }
+  } catch (error) {
+    console.log("email not register");
+    res.render("sign-in",{created:""});
+
+  }
+});
+
+
+//dashboard render
+router.get("/dashboard", (req, res) => {
+  RegisterSchema.findOne({_id:req.session.userId},function(err,data){
+		if(!data){
+      res.render("sign-in",{created:""});
+		}else{
+			console.log("found session");
+      res.render("dashboard");
+		}
+});
+});
+
+//profile render
+router.get("/profilementee", (req, res) => {
+console.log("inside profile");
+	RegisterSchema.findOne({_id:req.session.userId},function(err,data){
+		if(!data){
+      res.render("sign-in",{created:""});
+		}else{
+			//console.log("found");
+      console.log("user profile name and id is");
+      console.log(data.name);
+      console.log(data._id);
+      res.render("profilementee", { profileobject: data });
+		}
+	});
+});
+// logout
+router.get('/logout',(req,res) => {
+  req.session.destroy();
+  res.redirect('/');
+});
+
+module.exports = router;
